a3e1dd704280b10af4c8a4f402b97886d04794a3
gaurav4883 <gaurav.deol1600@gmail.com>
2023-01-25 23:58:47 +0545

    Logic of operations completed

---
+++ calculator/index.js
@@ -0,0 +1,29 @@
+let screen = document.getElementById('our-screen')
+let buttons = document.querySelectorAll('button')
+let screenDisplay = '';
+
+
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        if (buttonText == 'x') {
+            buttonText = "*";
+            screenDisplay += buttonText;
+            screen.value += buttonText
+        }
+
+        else if (buttonText == "C") {
+            screenDisplay = "";
+            screen.value = ""
+        }
+        else if (buttonText == "=") {
+            screen.value = eval(screenDisplay)
+            screenDisplay = "";
+        }
+        else {
+            screenDisplay += buttonText
+            screen.value = screenDisplay;
+        }
+    })
+
+}
